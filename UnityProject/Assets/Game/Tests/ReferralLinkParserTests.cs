ee54154ed2e774ca2eee8de55976a53674f4e738
PanishevQA <sajber15@mail.ru>
2026-09-15 22:51:17 +0700

    test: cover long L3 referral links and malformed tokens

--- UnityProject/Assets/Game/Tests/ReferralLinkParserTests.cs
+++ UnityProject/Assets/Game/Tests/ReferralLinkParserTests.cs
@@ -1,3 +1,5 @@
+using System;
+using DontGetSidetracked.Gameplay;
 using DontGetSidetracked.Social;
 using NUnit.Framework;
 
@@ -13,6 +15,23 @@ namespace DontGetSidetracked.Tests
             Assert.That(referralId, Is.EqualTo(expected));
         }
 
+        [Test]
+        public void ParsesMaximumCurrentL3ChallengeToken()
+        {
+            var date = new DateTime(2026, 9, 15, 0, 0, 0, DateTimeKind.Utc);
+            string token = OfflineChallengeCodec.Encode(
+                date,
+                123456,
+                RouteGenerator.CurrentGeneratorVersion,
+                3,
+                new[] { 4200, 3600, 2900 },
+                91.2);
+
+            Assert.That(token.Length, Is.EqualTo(36));
+            Assert.That(ReferralLinkParser.TryParse("nesbeisya://challenge/" + token, out string referralId), Is.True);
+            Assert.That(referralId, Is.EqualTo(token));
+        }
+
         [TestCase("")]
         [TestCase("nesbeisya://settings")]
         [TestCase("nesbeisya://challenge/../bad")]
@@ -20,6 +39,18 @@ namespace DontGetSidetracked.Tests
         public void RejectsUnsupportedLinks(string input)
         {
             Assert.That(ReferralLinkParser.TryParse(input, out _), Is.False);
+        }
+
+        [TestCase("L3")]
+        [TestCase("L320260915")]
+        [TestCase("L32026091500000001013FFFF3E8")]
+        public void CodecRejectsMalformedL3WithoutThrowing(string token)
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                bool decoded = OfflineChallengeCodec.TryDecode(token, out _);
+                Assert.That(decoded, Is.False);
+            });
         }
     }
 }
