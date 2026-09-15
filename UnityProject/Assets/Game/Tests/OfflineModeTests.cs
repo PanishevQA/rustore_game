ffbe545946213d3204d909d3681216aedd43203c
PanishevQA <sajber15@mail.ru>
2026-09-15 19:28:20 +0700

    fix: import core replay types in offline tests

--- UnityProject/Assets/Game/Tests/OfflineModeTests.cs
+++ UnityProject/Assets/Game/Tests/OfflineModeTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DontGetSidetracked.Core;
 using DontGetSidetracked.Gameplay;
 using DontGetSidetracked.Services;
 using DontGetSidetracked.Social;
