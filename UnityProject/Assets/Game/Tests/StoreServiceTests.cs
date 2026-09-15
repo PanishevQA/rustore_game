d274783774e7c0012624eb0340c1ca8f077d32cf
PanishevQA <sajber15@mail.ru>
2026-09-15 15:01:45 +0700

    test: cover purchases and entitlement restore

---
+++ UnityProject/Assets/Game/Tests/StoreServiceTests.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DontGetSidetracked.Core;
+using DontGetSidetracked.Economy;
+using DontGetSidetracked.Services;
+using NUnit.Framework;
+
+namespace DontGetSidetracked.Tests
+{
+    public sealed class StoreServiceTests
+    {
+        [Test]
+        public async Task HintsPurchase_IsGrantedOncePerPurchaseId()
+        {
+            var save = SaveData.CreateNew();
+            var repo = new MemorySaveRepository(save);
+            var payments = new FakePayments
+            {
+                PurchaseResult = new PaymentPurchaseResult(PurchaseOutcome.Completed, ProductIds.Hints10, "purchase_1")
+            };
+            var store = new StoreService(payments, repo, save);
+
+            StorePurchaseResult first = await store.PurchaseAsync(ProductIds.Hints10);
+            StorePurchaseResult second = await store.PurchaseAsync(ProductIds.Hints10);
+
+            Assert.That(first.GrantApplied, Is.True);
+            Assert.That(second.GrantApplied, Is.False);
+            Assert.That(save.Hints, Is.EqualTo(10));
+        }
+
+        [Test]
+        public async Task CancelledPurchase_DoesNotGrantEntitlement()
+        {
+            var save = SaveData.CreateNew();
+            var store = new StoreService(
+                new FakePayments
+                {
+                    PurchaseResult = new PaymentPurchaseResult(PurchaseOutcome.Cancelled, ProductIds.RemoveAds)
+                },
+                new MemorySaveRepository(save),
+                save);
+
+            StorePurchaseResult result = await store.PurchaseAsync(ProductIds.RemoveAds);
+
+            Assert.That(result.GrantApplied, Is.False);
+            Assert.That(store.InterstitialsRemoved, Is.False);
+        }
+
+        [Test]
+        public async Task RestoreStarterPack_GrantsNoAdsAndThreeSkins()
+        {
+            var save = SaveData.CreateNew();
+            var payments = new FakePayments
+            {
+                Restored = new[] { ProductIds.StarterPack }
+            };
+            var store = new StoreService(payments, new MemorySaveRepository(save), save);
+
+            int changed = await store.RestoreAsync();
+
+            Assert.That(changed, Is.EqualTo(1));
+            Assert.That(store.InterstitialsRemoved, Is.True);
+            Assert.That(save.Inventory, Does.Contain(ProductIds.SkinNeon));
+            Assert.That(save.Inventory, Does.Contain(ProductIds.SkinRetro));
+            Assert.That(save.Inventory, Does.Contain("skin_gold"));
+        }
+
+        private sealed class MemorySaveRepository : ISaveRepository
+        {
+            private SaveData _save;
+            public MemorySaveRepository(SaveData save) => _save = save;
+            public SaveData Load() => _save;
+            public void Save(SaveData data) => _save = data;
+        }
+
+        private sealed class FakePayments : IPaymentService
+        {
+            public PaymentPurchaseResult PurchaseResult =
+                new PaymentPurchaseResult(PurchaseOutcome.Failed, "none");
+            public IReadOnlyList<string> Restored = new string[0];
+
+            public Task<IReadOnlyList<StoreProduct>> GetProductsAsync(IReadOnlyList<string> productIds) =>
+                Task.FromResult<IReadOnlyList<StoreProduct>>(new StoreProduct[0]);
+
+            public Task<PaymentPurchaseResult> PurchaseAsync(string productId) =>
+                Task.FromResult(PurchaseResult);
+
+            public Task<IReadOnlyList<string>> RestoreEntitlementsAsync() =>
+                Task.FromResult(Restored);
+        }
+    }
+}
