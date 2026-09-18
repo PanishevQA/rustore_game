d4a50431f19cf12255d7121fc2a636204dd68dfb
PanishevQA <sajber15@mail.ru>
2026-09-18 22:12:42 +0700

    Give notification prompt release visual ownership

--- UnityProject/Assets/Game/Presentation/RuntimePlatformCoordinator.cs
+++ UnityProject/Assets/Game/Presentation/RuntimePlatformCoordinator.cs
@@ -347,6 +347,10 @@ namespace DontGetSidetracked.Presentation
             scaler.referenceResolution = new Vector2(1080, 1920);
             scaler.matchWidthOrHeight = 0.5f;
 
+            var releaseVisual = new GameObject("ReleaseVisual", typeof(RectTransform));
+            releaseVisual.transform.SetParent(canvasGo.transform, false);
+            ReleaseUiKit.Stretch(releaseVisual.GetComponent<RectTransform>());
+
             var backdrop = new GameObject("Backdrop", typeof(RectTransform), typeof(Image));
             backdrop.transform.SetParent(canvasGo.transform, false);
             ReleaseUiKit.Stretch(backdrop.GetComponent<RectTransform>());
